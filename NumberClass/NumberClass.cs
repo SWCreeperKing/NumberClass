4ab9d109a7b156955f41f292e82ea2f9442828b4
SWCreeperKing <SW.CreeperKing@gmail.com>
2024-10-31 16:34:07 -0700

    TESTS PASS

--- NumberClass/NumberClass.cs
+++ NumberClass/NumberClass.cs
@@ -1,14 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
+using static NumberClass;
+using Double = System.Double;
 
 public readonly struct NumberClass
 {
+    private static readonly Dictionary<string, (double Mantissa, double Exponent, UInt128 Magnitude)> StringCache =
+        new();
+
+    private static readonly Regex NormalSyn = new(@"(e*)([\d.,]*e{1}[\d.,]*e{0,1}[\d.,]*)$");
+    private static readonly Regex ComplexSyn = new(@"([\d.,]*)e{1}([\d.,]*e{0,1})([\d.,]*)$");
+    private static readonly Regex MagSyn = new(@"([\d,]*)f([\d.,]*e{1}[\d.,]*e{0,1}[\d.,]*)$");
+
     public const double MagnitudeLimiter = 1e100;
     public const int U128Exponent = 38;
-    public static readonly double U128Double = (double) UInt128.MaxValue;
-
+    public static readonly double U128Double = (double)UInt128.MaxValue;
+    public static readonly NumberClass NegInf = new(double.NegativeInfinity, double.NegativeInfinity);
+    public static readonly NumberClass Inf = new(double.PositiveInfinity, double.PositiveInfinity);
+    public static readonly NumberClass NaN = new(double.NaN, double.NaN);
     public static readonly NumberClass MaxMagnitudeValue = new(9.99, double.MaxValue, UInt128.MaxValue);
     public static readonly NumberClass MaxValue = new(9.99, double.MaxValue);
     public static readonly NumberClass Double = new(double.MaxValue);
@@ -16,8 +29,11 @@ public readonly struct NumberClass
     public static readonly NumberClass Long = new(long.MaxValue);
     public static readonly NumberClass Int = new(int.MaxValue);
     public static readonly NumberClass E = new(Math.E);
+    public static readonly NumberClass Ten = new(1, 1);
     public static readonly NumberClass One = new(1);
     public static readonly NumberClass Zero = new();
+    public static readonly NumberClass NegOne = new(-1);
+    public static readonly NumberClass NegTwo = new(-2);
 
     public static string Formatter = SciNotation.Name;
 
@@ -29,7 +45,7 @@ public readonly struct NumberClass
 
     public double Mantissa { get; init; }
     public double Exponent { get; init; }
-    public UInt128 Magnitude { get; init; }
+    public UInt128 Magnitude { get; init; } // 340,282,366,920,938,463,463,374,607,431,768,211,456
 
     public NumberClass(double mantissa = 0, double exponent = 0, UInt128 magnitude = default)
     {
@@ -39,37 +55,82 @@ public readonly struct NumberClass
     public NumberClass(NumberClass nc)
         => (Mantissa, Exponent, Magnitude) = Update(nc.Mantissa, nc.Exponent, nc.Magnitude);
 
-    // todo rank functionality later
+    /// <summary>
+    /// Supported Formatting <br />
+    /// M = mantissa (double)<br />
+    /// N = exponent's mantissa (double, 1 by default)<br />
+    /// X = exponent (double)<br />
+    /// L = layer (non-negative integer, up to 2^128 <br />
+    /// or 340,282,366,920,938,463,463,374,607,431,768,211,456) <br />
+    /// base 'A' formatting:
+    /// <list type="bullet">
+    /// <item>M => M</item>
+    /// <item>eX => 10^X</item>
+    /// <item>Me => M * 10 </item>
+    /// <item>MeX => M * 10 ^ X </item>
+    /// <item>eeX => 10 ^ (1 * 10 ^ X) </item>
+    /// <item>MeeX => M * 10 ^ (1 * 10 ^ X) </item>
+    /// <item>MeNeX => M * 10 ^ (N * 10 ^ X) </item>
+    /// </list>
+    ///
+    /// extended formatting:
+    /// <list type="bullet">
+    /// <item>e... (N e's) A => 10^... (N 10^s) A</item> 
+    /// <item>LfA => 10^... (L 10^s) A</item> 
+    /// </list>>
+    /// note: <br />
+    /// fM or fX is not allowed, do fMe or FeX <br />
+    /// eeeX may cause problems
+    /// </summary>
     public NumberClass(string s)
     {
+        if (StringCache.TryGetValue(s, out var cached))
+        {
+            (Mantissa, Exponent, Magnitude) = cached;
+            return;
+        }
+
+        s = s.ToLower();
+        var rawS = s;
         double man, exp = 0;
+        UInt128 mag = 0;
 
-        double Parse(Span<char> text)
+        if (s.Contains('f'))
         {
-            return text.ToString() == "" ? 1 : double.Parse(text.ToString(), CultureInfo.InvariantCulture);
+            var fLoc = s.IndexOf('f');
+            mag = UInt128.Parse(s[..fLoc], CultureInfo.CurrentCulture);
+            (man, exp) = ParseComplex(s[(fLoc + 1)..]);
         }
-
-        var span = new Span<char>(s.ToLower().ToCharArray());
-        if (span.Contains('e'))
+        else if (s.Contains('e'))
         {
-            var e = s.Count(c => c == 'e');
-            if (e > 2) throw new Exception("NumberClass: string can only have 2 `e`s");
-            var first = span.IndexOf('e');
-            if (e == 2)
-            {
-                var last = span.LastIndexOf('e');
-                man = Parse(span[..first]);
-                exp = Parse(span[(first + 1)..last]) * Math.Pow(10, Parse(span[(last + 1)..]));
-            }
-            else (man, exp) = (Parse(span[..first]), Parse(span[(first + 1)..]));
+            var match = NormalSyn.Match(s).Groups;
+            mag = (UInt128) match[1].Value.Count(c => c == 'e');
+            (man, exp) = ParseComplex(match[2].Value);
         }
-        else man = double.Parse(s, CultureInfo.InvariantCulture);
+        else man = Parse(s);
 
-        (Mantissa, Exponent, Magnitude) = Update(man, exp);
+        StringCache[rawS] = (Mantissa, Exponent, Magnitude) = Update(man, exp, mag);
+        return;
+
+        double Parse(string text)
+            => double.TryParse(text, CultureInfo.CurrentCulture, out var d)  ? d : 1;
+
+        (double man, double exp) ParseComplex(string s)
+        {
+            var regRes = ComplexSyn.Match(s).Groups;
+            var man = Parse(regRes[1].Value);
+            var expMan = Parse(regRes[2].Value);
+            var exp = regRes[3].Value != "" ? Parse(regRes[3].Value[..^1]) : -1;
+
+            return exp == -1 ? (man, expMan) : (man, expMan * Math.Pow(10, exp));
+        }
     }
 
-    private static (double man, double exp, UInt128) Update(double man = 0, double exp = 0, UInt128 magnitude = default)
+    private static (double man, double exp, UInt128 mag) Update(double man = 0, double exp = 0,
+        UInt128 magnitude = default)
     {
+        if (double.IsNaN(man) || double.IsNaN(exp) || double.IsInfinity(man) || double.IsInfinity(exp))
+            return (man, exp, magnitude);
         if (man == 0) return (0, 0, 0);
 
         var isNeg = man < 0;
@@ -108,7 +169,7 @@ public readonly struct NumberClass
     public UInt128 MinMagnitude(NumberClass nc) => nc.Magnitude < Magnitude ? nc.Magnitude : Magnitude;
 
     public double MagnitudeDelta(NumberClass nc)
-        => (double) (Magnitude > nc.Magnitude ? Magnitude - nc.Magnitude : nc.Magnitude - Magnitude);
+        => (double)(Magnitude > nc.Magnitude ? Magnitude - nc.Magnitude : nc.Magnitude - Magnitude);
 
     #region compare operators
 
@@ -217,33 +278,42 @@ public readonly struct NumberClass
         return new NumberClass(n1.Mantissa / n2.Mantissa, exponent);
     }
 
-    public NumberClass Pow(NumberClass n)
+    public NumberClass Pow(NumberClass n) // from https://github.com/Patashu/break_eternity.js
     {
-        if (n == One || this == One || this == Zero) return this;
-        if (n == Zero) return One;
-        if (Exponent == 0 && n.Exponent == 0) return Math.Pow(Mantissa, n.Mantissa);
+        // if (n == One || this == One || this == Zero) return this;
+        // if (n == Zero) return One;
 
-        if (Magnitude > 0 || n.Magnitude > 0)
+        var ret = (Abs().Log10() * n).Pow10WithThis();
+        if (IsNeg() && int.Parse($"{$"{n.Mantissa}"[^1]}") % 2 != 0) return ret.Negate();
+        return ret;
+    }
+
+    public NumberClass Pow10WithThis()
+    {
+        if (Exponent < -15) return Zero;
+        var man = Mantissa;
+        var expo = Exponent;
+
+        if (Exponent < 0)
         {
-            // todo: find a way to do this /shrug
-            Console.WriteLine("EXECUTED ADVANCED POWER, WIP RETURNING MAX");
-            return Max(n);
+            man /= Math.Pow(10, Math.Abs(Exponent));
+            expo = 0;
         }
 
-        var tempExpo = Exponent + Math.Log10(Mantissa);
+        var trunc = double.Truncate(man);
 
-        if (Math.Max(Math.Log10(Exponent), 0) + n.Exponent >= 300)
+        if (trunc > 0)
         {
-            var exponent1 = Math.Log10(tempExpo);
-            var exponent2 = n.Exponent + Math.Log10(n.Exponent);
-
-            return new NumberClass(Mantissa, exponent1 + exponent2);
+            var tExpo = Math.Floor(Math.Log10(trunc));
+            trunc /= Math.Pow(10, tExpo);
+            expo += tExpo;
         }
 
-        tempExpo *= n.GetRealMantissa();
-        return tempExpo < 1e17
-            ? new NumberClass(Math.Pow(10, tempExpo % 1), Math.Floor(tempExpo), Magnitude)
-            : new NumberClass(Mantissa, tempExpo, Magnitude);
+        var leftExpo = expo % 100;
+        var newExpo = trunc * Math.Pow(10, leftExpo);
+        var num = new NumberClass(Math.Round(Math.Pow(10, man - trunc), 12), newExpo,
+            Magnitude + (UInt128)(expo - leftExpo) / 100);
+        return num;
     }
 
     public NumberClass Root(long rootBase)
@@ -263,20 +333,15 @@ public readonly struct NumberClass
     public NumberClass Cbrt() => Root(3);
 
     public NumberClass Log() => Log(E);
+
     public NumberClass Log(NumberClass logBase) => this == Zero ? Zero : Log10() / logBase.Log10();
+
     public NumberClass Log2() => Log(2);
 
-    public NumberClass Log10()
-    {
-        if (Magnitude > 0)
-        {
-            // todo: find a way to do this /shrug
-            Console.WriteLine("EXECUTED ADVANCED POWER, WIP RETURNING MAX");
-            return this;
-        }
-
-        return Exponent + Math.Log10(Mantissa);
-    }
+    public NumberClass Log10() =>
+        Magnitude > 1
+            ? new NumberClass(Exponent + Math.Log10(Mantissa), 1, Magnitude - 1)
+            : Exponent + Math.Log10(Mantissa);
 
     #endregion
 
@@ -288,13 +353,13 @@ public readonly struct NumberClass
     public static explicit operator int(NumberClass n)
     {
         if (n > Int) return int.MaxValue;
-        return (int) (n.Mantissa * Math.Pow(10, n.Exponent));
+        return (int)(n.Mantissa * Math.Pow(10, n.Exponent));
     }
 
     public static explicit operator long(NumberClass n)
     {
         if (n > Long) return long.MaxValue;
-        return (long) (n.Mantissa * Math.Pow(10, n.Exponent));
+        return (long)(n.Mantissa * Math.Pow(10, n.Exponent));
     }
 
     public static explicit operator double(NumberClass n)
@@ -306,7 +371,7 @@ public readonly struct NumberClass
     public static explicit operator float(NumberClass n)
     {
         if (n > Float) return float.MaxValue;
-        return (float) (n.Mantissa * Math.Pow(10, n.Exponent));
+        return (float)(n.Mantissa * Math.Pow(10, n.Exponent));
     }
 
     #endregion
--- NumberClassTester/Program.cs
+++ NumberClassTester/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Numerics;
+using System.Runtime.InteropServices;
 using Newtonsoft.Json;
 using NUnit.Framework;
 
@@ -12,6 +12,8 @@ public class Program
         public NumberClass n2;
         public NumberClass n3;
         public NumberClass n4;
+        public NumberClass n5;
+        public NumberClass n6;
 
         [SetUp]
         public void Setup()
@@ -20,6 +22,8 @@ public class Program
             n2 = "1e50";
             n3 = new NumberClass(1, 1e99);
             n4 = new NumberClass(1, 101, 1);
+            n5 = new NumberClass(1, 2, 3).Log10();
+            n6 = new NumberClass(1, 2, 2);
         }
 
         [Test]
@@ -27,33 +31,38 @@ public class Program
         {
             NumberClass? nc1 = 6;
             NumberClass? nc2 = null;
-            Assert.IsNotNull(nc1);
-            Assert.IsNull(nc2);
+            Assert.That(nc1, Is.Not.Null);
+            Assert.That(nc2, Is.Null);
         }
 
         [Test]
         public void PlusPlus()
         {
             NumberClass n = 1;
-            Assert.True(n == 1);
+            Assert.That(n == 1, Is.True);
             n++;
-            Assert.True(n == 2 && ++n == 3);
+            Assert.That(n == 2, Is.True);
+            Assert.That(++n == 3, Is.True);
         }
 
         [Test]
         public void LogTest()
         {
-            Assert.True(n1.Log10() == 4 && n2.Log10() == 50 && n2.Log(n1) == 12.5);
-            Assert.True(n2.Log().GetRealMantissa() == 115.12925464970229);
+            Assert.That(n1.Log10() == 4, Is.True);
+            Assert.That(n2.Log10() == 50, Is.True);
+            Assert.That(n2.Log(n1) == 12.5, Is.True);
+            Console.WriteLine(n2.Log().GetRealMantissa());
+            Assert.That(Math.Abs(n2.Log().GetRealMantissa() - 115.12925464970229f) < .00001, Is.True);
         }
 
         [Test]
         public void AddTest()
         {
-            Assert.True(n1 + n2 == new NumberClass(1, 50));
-            var preAdd = new NumberClass("1e30") + "6e29";
+            Assert.That(n1 + n2 == new NumberClass(1, 50), Is.True);
+            var prePreAdd = new NumberClass("6e29");
+            var preAdd = new NumberClass("1e30") + prePreAdd;
             var addRes = new NumberClass(1.6f, 30);
-            Assert.True(preAdd == addRes);
+            Assert.That(preAdd == addRes, Is.True);
         }
 
         [Test]
@@ -63,52 +72,55 @@ public class Program
             var sub1Res = new NumberClass(-1, 50);
             var sub2 = n2 - n1;
             var sub2Res = new NumberClass(1, 50);
-            Assert.True(sub1 == sub1Res);
-            Assert.True(sub2 == sub2Res);
-            Assert.True(NumberClass.One / 2 == .5f);
+            Assert.That(sub1 == sub1Res, Is.True);
+            Assert.That(sub2, Is.EqualTo(sub2Res));
+            Assert.That(NumberClass.One / 2 == .5f, Is.True);
+
             var sub3n1 = new NumberClass(2.2561784573198107, 6);
             var sub3n2 = new NumberClass(1.9603208295529388, 7);
             var sub3 = sub3n1 - sub3n2;
             var sub3Res = new NumberClass(-1.73470298382095773, 7);
             Console.WriteLine(sub3.ToExactString());
             Console.WriteLine(sub3Res.ToExactString());
-            Assert.True(sub3 == sub3Res);
+            Assert.That(sub3 == sub3Res, Is.True);
         }
 
         [Test]
         public void MultiTest()
         {
-            Assert.True(n1 * n2 == new NumberClass(1, 54));
+            Assert.That(n1 * n2 == new NumberClass(1, 54), Is.True);
         }
 
         [Test]
         public void DivTest()
         {
-            Assert.True(n1 / n2 == new NumberClass(1, -46));
-            Assert.True(n2 / n1 == new NumberClass(1, 46));
+            Assert.That(n1 / n2 == new NumberClass(1, -46), Is.True);
+            Assert.That(n2 / n1 == new NumberClass(1, 46), Is.True);
         }
 
         [Test]
         public void PowerTest()
         {
-            Console.WriteLine(n1.Pow(n2));
-            Assert.True(n1.Pow(n2) == new NumberClass(1, 4e50));
-            Assert.True(n2.Pow(n1) == new NumberClass(1, 50e4));
+            // Assert.That(new NumberClass(2).Pow(2) == 4, Is.True);
+            // Assert.That(new NumberClass(4).Pow(4) == 256, Is.True);
+            Console.WriteLine(n1.Pow(n2).ToExactString());
+            Assert.That(n1.Pow(n2) == new NumberClass(1, 4e50), Is.True);
+            Assert.That(n2.Pow(n1) == new NumberClass(1, 50e4), Is.True);
         }
 
         [Test]
         public void CompareTest()
         {
-            Assert.False(n1 > n2);
-            Assert.True(n1 < n2);
-            Assert.True(n1 != n2);
-            Assert.False(n1 == n2);
+            Assert.That(n1 > n2, Is.False);
+            Assert.That(n1 < n2, Is.True);
+            Assert.That(n1 != n2, Is.True);
+            Assert.That(n1 == n2, Is.False);
         }
 
         [Test]
         public void RootTest()
         {
-            Assert.True(new NumberClass(3).Pow(4).Root(4) == 3);
+            Assert.That(new NumberClass(3).Pow(4).Root(4) == 3, Is.True);
         }
 
         [Test]
@@ -119,43 +131,43 @@ public class Program
             Console.WriteLine(n2);
             Console.WriteLine(jsonString);
             Console.WriteLine(n2Converted);
-            Assert.True(n2Converted == n2);
+            Assert.That(n2Converted == n2, Is.True);
         }
 
         [Test]
         public void MagnitudeTest()
         {
-            Assert.True(n1.Magnitude == 0);
-            Assert.True(n2.Magnitude == 0);
-            Assert.True(n3.Magnitude == 0);
-            Assert.True(n4.Magnitude == 1);
+            Assert.That(n1.Magnitude == 0, Is.True);
+            Assert.That(n2.Magnitude == 0, Is.True);
+            Assert.That(n3.Magnitude == 0, Is.True);
+            Assert.That(n4.Magnitude == 1, Is.True);
         }
 
         [Test]
         public void MagnitudeAdd()
         {
-            Assert.True((n3 + n3).ToExactString() == "2_1e99_0");
-            Assert.True((n3 + n4).ToExactString() == "1.01_101_1");
+            Assert.That((n3 + n3).ToExactString() == "2_1e99_0", Is.True);
+            Assert.That((n3 + n4).ToExactString() == "1.01_101_1", Is.True);
         }
 
         [Test]
         public void MagnitudeSubtract()
         {
-            Assert.True((n3 - n3).ToExactString() == "0_0_0");
-            Assert.True((n3 - n4).ToExactString() == "-9.9_100_1");
-            Assert.True((n4 - n3).ToExactString() == "9.9_100_1");
+            Assert.That((n3 - n3).ToExactString() == "0_0_0", Is.True);
+            Assert.That((n3 - n4).ToExactString() == "-9.9_100_1", Is.True);
+            Assert.That((n4 - n3).ToExactString() == "9.9_100_1", Is.True);
         }
 
         [Test]
         public void MagnitudeMultiply()
         {
-            Assert.True((n3 * n4).ToRawString() == "1_200_1");
+            Assert.That((n3 * n4).ToRawString() == "1_200_1", Is.True);
         }
 
         [Test]
         public void MagnitudeDivide()
         {
-            Assert.True((n3 / n4).ToRawString() == "1_-2_0");
+            Assert.That((n3 / n4).ToRawString() == "1_-2_0", Is.True);
         }
 
         [Test]
@@ -163,7 +175,7 @@ public class Program
         {
             Console.WriteLine(n3.Pow(n4).ToRawString());
         }
-        
+
         /*
         NumberClass b = 2;
 
